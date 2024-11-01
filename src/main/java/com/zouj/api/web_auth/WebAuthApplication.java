2e894930f59ebc14adea5f673592a72022d439e6
Jagger <ulrichjagger@gmail.com>
2024-11-01 12:38:07 +0100

    initial commit

---
+++ src/main/java/com/zouj/api/web_auth/WebAuthApplication.java
@@ -0,0 +1,13 @@
+package com.zouj.api.web_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAuthApplication.class, args);
+	}
+
+}
