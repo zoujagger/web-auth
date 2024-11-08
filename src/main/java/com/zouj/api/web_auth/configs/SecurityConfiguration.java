45eeb3ecb0f2c7dbfda6b853bd53d0aeeadfc43b
Jagger <ulrichjagger@gmail.com>
2024-11-08 12:31:01 +0100

    enblae the method security to perform role access in controller level

--- src/main/java/com/zouj/api/web_auth/configs/SecurityConfiguration.java
+++ src/main/java/com/zouj/api/web_auth/configs/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,6 +17,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfiguration {
 
     private final AuthenticationProvider authenticationProvider;
